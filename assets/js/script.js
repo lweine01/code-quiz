4f3e081c7fb206dfe649da6db4d7a2b8ae6ebce5
lweine01 <laurarweiner@gmail.com>
2021-08-24 17:22:14 -0400

    Created nextQuestion function and endQuiz function, added event listener to uLeL, added form to html and added event listener to button, started to send input and score to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,120 @@
 var multipleChoices = [
     {
-    question: "question",
+    question: "question1",
     answers: ["a1", "a2", "a3", "a4"],
     correctAnswer: 0,
     },
     {
-    question: "question",
-    answers: ["a1", "a2", "a3", "a4"],
+    question: "question2",
+    answers: ["b1", "b2", "b3", "b4"],
     correctAnswer: 1,
     },
     {
-    question: "question",
-    answers: ["a1", "a2", "a3", "a4"],
+    question: "question3",
+    answers: ["c1", "c2", "c3", "c4"],
     correctAnswer: 2,
     },
     {
-    question: "question",
-    answers: ["a1", "a2", "a3", "a4"],
+    question: "question4",
+    answers: ["d1", "d2", "d3", "d4"],
     correctAnswer: 3,
     },
     {
-    question: "question",
-    answers: ["a1", "a2", "a3", "a4"],
+    question: "question5",
+    answers: ["e1", "e2", "e3", "e4"],
     correctAnswer: 0,
     }
 ]
 
 var startButton = document.querySelector("#start");
-var question = document.querySelector("#question");
-var answers = document.querySelectorAll("li");
+var questionEl = document.querySelector("#question");
 var startPage = document.querySelector("#startPage");
 var timer = document.querySelector("#timer");
+var ulEl = document.querySelector("ul")
+var results = document.querySelector("#result");
 var timeLeft = 60;
+var counter = 0;
+var score = 0;
+var targetQuestion = multipleChoices[counter];
+var questionTab = document.querySelector("#questionTab");
+var formField = document.querySelector("form");
+var button = document.querySelector("button");
+var input = document.querySelector("input");
+
+formField.style.display="none";
 
 
 startButton.addEventListener("click", function(){
     startPage.style.display = "none";
     setTimer ();
+    
+    questionEl.textContent = targetQuestion.question;
+    
+    for (var i = 0; i < targetQuestion.answers.length; i++){
+        var answersEl = document.createElement("li");
+        answersEl.textContent = targetQuestion.answers[i];
+        ulEl.appendChild(answersEl);
+        }
+});
 
-    for(i=0; i < multipleChoices.length; i++) {
-        var element = multipleChoices[i];
+ulEl.addEventListener("click", function(event){
+    var selectedAnswer = event.target;
+    console.log(selectedAnswer.textContent);
+    if(selectedAnswer.textContent===targetQuestion.answers[targetQuestion.correctAnswer]) {
+        results.textContent = "You are correct!";
+        score++;
+    } else {
+        results.textContent = "Sorry, you are incorrect.";
+        timeLeft-=5;
     }
-})
+    nextQuestion(); 
+});
+
+var nextQuestion = function(){
+    ulEl.innerHTML="";
+    counter++;
+    targetQuestion = multipleChoices[counter];
+    if(!targetQuestion){
+        timeLeft=0;
+        return;
+    }
+    questionEl.textContent = targetQuestion.question;
+    
+    for (var i = 0; i < targetQuestion.answers.length; i++){
+        var answersEl = document.createElement("li");
+        answersEl.textContent = targetQuestion.answers[i];
+        ulEl.appendChild(answersEl);
+        }
+}
 
 var setTimer = function(){
     timer.textContent = "Time: " + timeLeft;
     var timerInterval = setInterval(function(){
         timeLeft--;
-        timer.textContent = "Time: " + timeLeft;
-        if (timeLeft ===0) {
+        if (timeLeft <=0) {
+            timeLeft=0;
             clearInterval(timerInterval);
+            console.log("quizOver");
             quizOver ();
         }
+        timer.textContent = "Time: " + timeLeft;
     },1000);
+}
+
+var quizOver = function(){
+    questionEl.innerHTML="";
+    results.innerHTML="";
+
+    var scoreDisplay = document.querySelector("#score");
+    scoreDisplay.textContent = "Your final score is " + score + "."
+
+    formField.style.display = "block";
 
 }
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+    var inputSubmission = input.value;
+    localStorage.setItem("initials", inputSubmission);
+    localStorage.setItem("score", score);
+})
